5f8a63f49751a8654dff7b2ca3962a961563b6c0
hardikjha97 <kumarhardik436@gmail.com>
2026-02-18 17:05:35 +0530

    server and user model setup

---
+++ backend/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  favorites: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Product" }
+  ]
+});
+
+module.exports = mongoose.model("User", userSchema);
